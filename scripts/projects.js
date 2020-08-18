eca10669d361d98c474675b0b65417c95fa57fc1
Graham Power <powergraham907@gmail.com>
2020-08-18 17:13:19 -0400

    small changes to project page design

--- scripts/projects.js
+++ scripts/projects.js
@@ -6,8 +6,13 @@ $(function () {
         //console.log(content.css('max-height'));
         if(content.style.maxHeight){
             content.style.maxHeight = null;
+            //content.style.marginTop = 0
+            setTimeout(function() {
+                content.style.marginTop = 0;
+            }, 500);
         } else {
-            content.style.maxHeight = content.scrollHeight + 'px';
+            content.style.maxHeight = content.scrollHeight + 'px';            
+            content.style.marginTop = '4.5rem';
         }
         
     });
