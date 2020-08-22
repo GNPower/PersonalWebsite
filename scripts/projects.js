3fcb84e4e975b7e1f915aab68d9ba2163b9d1898
Graham Power <powergraham907@gmail.com>
2020-08-22 14:33:25 -0400

    projects page content injection

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,4 +1,27 @@
-$(function () {
+$.getJSON('/injections/projects.json', function (json) {
+
+    json.forEach(project => {
+        var html = '<div class="project"><div class="min flex-col"><div class="hero-content"><h1>' + project.title + '</h1></div></div><div class="max flex-col"><p class="border-fade">' + project.paragraph + '</p><div class="gallery">';
+
+        project.gallery.forEach(image => {
+            html = html + '<div class="img"><img src="' + image + '" alt=""></div>';
+        });
+                
+        html = html + '</div><div class="links"><h3 class="">Project Files</h3>';
+
+        project.links.forEach(link => {
+            html = html + '<div class="link"><div class="image"><img src="' + link.icon + '" alt="Project File"></div><div class="name"><h3>' + link.title + '</h3></div></div>';
+        });
+            
+        html = html + '</div></div></div>';
+
+        $('.projects-container').append(html);
+    });
+
+    makeExtendable();
+});
+
+function makeExtendable() {
 
     $('.project .min').click(function() {
 
@@ -16,4 +39,4 @@ $(function () {
         }
         
     });
-});
+}
